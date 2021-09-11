156a5f592542fef56546957043cca98bb0bd505a
linasa <lina.b4007@gmail.com>
2021-09-11 14:43:35 +0200

    3 bruits

--- testImages.py
+++ testImages.py
@@ -11,14 +11,32 @@ from random import randint, random
 img = io.imread("image2_reference.png")
 
 
-# ALGO SALT AND PEPPER
+# # ALGO SALT AND PEPPER
+# for line in range(len(img)):
+#     for col in range(len(img)):
+#         if (randint(1, 10) == 1):
+#             if (random() <0.5):
+#                 img[line, col] = 0
+#             else:
+#                 img[line, col] = 255
+
+# ALGO BRUIT ADDITIF
 for line in range(len(img)):
     for col in range(len(img)):
         if (randint(1, 10) == 1):
-            if (random() <0.5):
-                img[line, col] = 0
-            else:
-                img[line, col] = 255
+            img[line, col] = img[line, col] + 20
+            if (img[line, col]>255):
+              img[line, col] = 255
+
+                
+# # ALGO BRUIT MULTIPLICATIF
+# for line in range(len(img)):
+#     for col in range(len(img)):
+#         if (randint(1, 2) == 1):
+#             img[line, col] = img[line, col] * (1+2)
+#             if (img[line, col]>255):
+#               img[line, col] = 255                 
+
 
 #Affichage
 io.imshow(img)
