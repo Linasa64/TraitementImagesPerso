aa6f7195b086b812269aaec6cfb4aaf6e7fdbeae
linasa <lina.b4007@gmail.com>
2021-10-02 23:16:23 +0200

    Filtrage médian
    OK pour 9px

--- testDébruitage.py
+++ testDébruitage.py
@@ -9,8 +9,7 @@ import matplotlib.cm as cm
 from skimage import io
 
 
-img = io.imread("image1_bruitee_snr_10.8656.png")
-
+img = io.imread("image1_bruitee_snr_13.0913.png")
 
 def debruitageMoyenne(img):
     for line in range(1, (len(img)-2)):
@@ -21,7 +20,11 @@ def debruitageMediane(img):
     for line in range(1, (len(img)-2)):
         for col in range(1, len(img)-2):
             img[line, col] = mediane_pixels(line, col)
-
+            
+def debruitageMediane25(img):
+    for line in range(2, (len(img)-3)):
+        for col in range(2, len(img)-3):
+            img[line, col] = mediane_pixels25(line, col)
 
 def moyenne_pixels(x, y):
     sum = 0
@@ -38,14 +41,22 @@ def mediane_pixels(x, y):
         for col in range((y-1), (y+2)):
             liste.append(int(img[line, col]))
     liste.sort()
-    return liste[5]
+    return liste[4]
+
+def mediane_pixels25(x, y):
+    liste = []
+    for line in range((x-2), (x+3)):
+        for col in range((y-2), (y+3)):
+            liste.append(int(img[line, col]))
+    liste.sort()
+    return liste[12]
 
 def afficherCarre(x, y):
     for line in range((x-1), (x+2)):
         for col in range((y-1), (y+2)):
             print(img[line, col])
  
-            
+          
 debruitageMediane(img)
 
 ## CALCUL SNR
@@ -68,11 +79,6 @@ print("pSignal : ", pSignal)
 print("pBruit : ", pBruit)
 snr = 10*log((pSignal/pBruit), 10)
 print("SNR: ", snr)
-
-# debruitageMoyenne(img)
-# afficherCarre(10, 10)
-# print()
-# print(mediane_pixels(10, 10))
 
 
 #Affichage
